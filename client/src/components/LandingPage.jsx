0188c6526cf64072ae12daaf263b79039c2390b5
Steve Lemus <stv8904@hotmail.com>
2021-12-21 15:24:00 -0600

    LP changes

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
-import LPBG from "../assets/LPBG.mp4";
 
 export default function LandingPage() {
   return (
     <div>
-      <video autoPlay loop muted className={styles.video}>
-        <source src={LPBG} type="video/mp4" />
-      </video>
+      <div className={styles.video}>
+        <iframe
+          title="landingvideo"
+          height="100%"
+          width="100%"
+          position="absolute"
+          frameBorder="0"
+          src={
+            "https://www.youtube.com/embed/jP2ZQHrYRME?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&rel=0&autohide=1&modestbranding=1"
+          }
+          pointerEvents="none"
+        ></iframe>
+      </div>
       <h1 className={styles.title}>
         Welcome to <br /> Stevie's Dogs!
       </h1>
